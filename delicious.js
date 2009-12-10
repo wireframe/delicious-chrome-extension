3d7717b53b3fabc0621045168993bef79772341a
Ryan Sonnek <ryan@socialcast.com>
2009-12-09 23:20:04 -0600

    add background process

--- delicious.js
+++ delicious.js
@@ -1,52 +1,28 @@
+var delicious = {};
+
 /*
 post new bookmark to delicious
-
 see http://gist.github.com/7516
 */
-$(function() {
-  
-  function displayMessage(string) {
-    alert(string);
-  }
-  chrome.tabs.getSelected(null, function(tab) {
-    $('#url').val(tab.url);
- 		$('#description').val(tab.title);
- 	});
+delicious.saveBookmark = function(params, options) {
+  var defaults = {
+    type: "POST",
+    dataType: "xml",
+    url: "https://api.del.icio.us/v1/posts/add",
+    data: params,
+    error: function() {
+      console.error("Error saving bookmark to delicious");
+    },
+    success: function(xml) {
+      var result = $(xml).find("result");
+      var code = result.attr("code");
 
-  var submit = $('#submit');
-  $('#form').submit(function() {
-    $.ajax({
-      type: "POST",
-      dataType: "xml",
-      url: "https://api.del.icio.us/v1/posts/add",
-      data: $(this).serialize(),
-      beforeSend: function() {
-        submit.attr('disabled', 'disabled').text('Saving');
-      },
-      error: function() {
-        displayMessage("Error saving bookmark to delicious");
-      },
-      success: function(xml) {
-        var result = $(xml).find("result");
-        var code = result.attr("code");
-
-        if (code == "done") {
-          displayMessage("Bookmark saved to delicious!");
-        } else {
-          displayMessage("Error saving bookmark to delicious: " + code);
-        }
-      },
-      complete: function() {
-        submit.removeAttr('disabled').text('Save');
+      if (code == "done") {
+        console.log("Bookmark saved to delicious!");
+      } else {
+        console.error("Error saving bookmark to delicious: " + code);
       }
-    });
-
-    return false;
-  });
-});
-
-
-/*
-intercept user creating chrome bookmark
-http://code.google.com/chrome/extensions/bookmarks.html
-*/
+    }
+  };
+  $.ajax($.extend({}, defaults, options));
+};
