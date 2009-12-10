663e6c7c51d01700b91d57573b00dd8e559e56fb
Ryan Sonnek <ryan@socialcast.com>
2009-12-09 22:19:47 -0600

    listen for creating bookmarks

---
+++ delicious-chrome.js
@@ -0,0 +1,31 @@
+chrome.bookmarks.onCreated.addListener(function(id, BookmarkTreeNode bookmark) {
+  if (bookmark.url) {
+    var data = {
+      url: bookmark.url,
+      description: bookmark.title
+    };
+    
+    function displayMessage(string) {
+      alert(string);
+    }
+    $.ajax({
+      type: "POST",
+      dataType: "xml",
+      url: "https://api.del.icio.us/v1/posts/add",
+      data: data,
+      error: function() {
+        displayMessage("Error saving bookmark to delicious");
+      },
+      success: function(xml) {
+        var result = $(xml).find("result");
+        var code = result.attr("code");
+
+        if (code == "done") {
+          displayMessage("Bookmark saved to delicious!");
+        } else {
+          displayMessage("Error saving bookmark to delicious: " + code);
+        }
+      }
+    });
+  }
+});
