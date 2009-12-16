fc4161824cb9255d45e4040329fa928ef6a8d02f
Ryan Sonnek <ryan@socialcast.com>
2009-12-15 20:49:13 -0600

    add listener for removing bookmarks

--- delicious.js
+++ delicious.js
@@ -1,25 +1,37 @@
+/*
+api for interacting with delicious
+see see http://delicious.com/help/api
+*/
 var delicious;
 if (!delicious) {
   delicious = {};
 }
 
-/*
-post new bookmark to delicious
-see http://gist.github.com/7516
-*/
-delicious.saveBookmark = function(params, options) {
+delicious.request = function(options) {
   var defaults = {
     type: "GET",
     dataType: "xml",
-    url: "https://api.del.icio.us/v1/posts/add",
-    data: params,
     beforeSend: function(request) {
       var auth = 'Basic ' + btoa(localStorage['username'] + ':' + localStorage['password']);
       request.setRequestHeader('Authorization', auth);
     },
     error: function(request, status) {
-      console.error("Error saving bookmark to delicious: " + status);
-    },
+      console.error("Error connecting to delicious: " + status);
+    }
+  };
+  var opts = $.extend({}, defaults, options);
+  $.ajax(opts);
+};
+
+/*
+save a new bookmark to delicious.
+if bookmark already exists for this url, the existing one will be updated
+see http://delicious.com/help/api#posts_add
+*/
+delicious.saveBookmark = function(params, options) {
+  var defaults = {
+    url: "https://api.del.icio.us/v1/posts/add",
+    data: params,
     success: function(xml) {
       var result = $(xml).find("result");
       var code = result.attr("code");
@@ -31,6 +43,29 @@ delicious.saveBookmark = function(params, options) {
       }
     }
   };
-  options = $.extend({}, defaults, options);
-  $.ajax(options);
+  var opts = $.extend({}, defaults, options);
+  delicious.request(opts);
+};
+
+/*
+remove an existing bookmark from delicious
+see http://delicious.com/help/api#posts_delete
+*/
+delicious.removeBookmark = function(url) {
+  var defaults = {
+    url: "https://api.del.icio.us/v1/posts/add",
+    data: {url: url},
+    success: function(xml) {
+      var result = $(xml).find("result");
+      var code = result.attr("code");
+
+      if (code == "done") {
+        console.log("Bookmark removed from delicious.");
+      } else {
+        console.error("Error removing bookmark from delicious: " + code);
+      }
+    }
+  };
+  var opts = $.extend({}, defaults, options);
+  delicious.request(opts);
 };
